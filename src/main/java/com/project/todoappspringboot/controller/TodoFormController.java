dedbdf8673d65fe7435a34fbfff962ced5468dd5
Naufal Azim <naufal.azim@outlook.com>
2022-10-25 00:28:34 +0800

    Complete backend CRUD 1.0

--- src/main/java/com/project/todoappspringboot/controller/TodoFormController.java
+++ src/main/java/com/project/todoappspringboot/controller/TodoFormController.java
@@ -8,10 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Collections;
+import java.time.Instant;
+import java.time.ZoneId;
 
 @Controller
 public class TodoFormController {
@@ -22,12 +23,24 @@ public class TodoFormController {
     private TodoItemRepository todoItemRepository;
 
 
+    //Homepage ✅
+    @GetMapping("/")
+    public ModelAndView index() {
+        logger.debug("Get Request");
+        logger.info("Testing to get Request!");
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll()); //pulling all the items from db
+
+        modelAndView.addObject("today", Instant.now().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek());
+
+        return modelAndView;
+    }
+
     //    PART: ADD button ✅
     @GetMapping("/create-todo")
     public String showCreateForm(TodoItem todoItem) {
         return "add-todo-item";
     }
-
 
     //     PART: DELETE button ✅
     @GetMapping("/delete/{id}")
@@ -41,17 +54,41 @@ public class TodoFormController {
     }
 
 
-    //     PROBLEM HERE 🛑
+    //  ================   PROBLEM HERE 🛑   =================
+    // PROBLEM: My problem is when user click edit button the output is error: This application has no explicit mapping for /error, so you are seeing this as a fallback.
+
+    // ERROR DISPLAY:
+    // There was an unexpected error (type=Bad Request, status=400).
+    //  Required request parameter 'id' for method parameter type Long is not present
+
     //     PART: EDIT button 🛑
     @GetMapping("/edit/{id}")
-    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
-        TodoItem todoItem = todoItemRepository
-                .findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("id: "+id+ "not found"));
-
-        model.addAttribute("todo", todoItem);
-        return "update-todo-item";
+    public ModelAndView showUpdateForm(@PathVariable("id") Long id, Model model) {
+        ModelAndView modelAndView = new ModelAndView("update-to-do");
+        TodoItem todoItem = todoItemRepository.findById(id).get();
+        modelAndView.addObject("todo", todoItem);
+        return modelAndView;
     }
 
+    // PART: Save new data (update) 🆘
+    // update by id:
+    @PostMapping("/todo/{id}")
+    public String save(@ModelAttribute TodoItem todoItem) {
+        todoItem.setCreatedDate(Instant.now());
+        todoItem.setModifiedDate(Instant.now());
+        todoItemRepository.save(todoItem);
+
+        return "redirect:/";
+    }
+
+    // Update new data to database ✅
+    @PostMapping("/todo")
+    public String todoSave(@ModelAttribute TodoItem todoItem) {
+        todoItem.setCreatedDate(Instant.now());
+        todoItem.setModifiedDate(Instant.now());
+        todoItemRepository.save(todoItem);
+
+        return "redirect:/";
+    }
 
 }
